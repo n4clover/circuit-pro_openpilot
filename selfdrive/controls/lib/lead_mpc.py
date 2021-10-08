35afcbb4007a64ad160e571745fb306a8c694598
Circuit Pro Industries <oricalworks@gmail.com>
2021-10-08 08:12:13 -0400

    Update lead_mpc.py

--- selfdrive/controls/lib/lead_mpc.py
+++ selfdrive/controls/lib/lead_mpc.py
@@ -10,10 +10,10 @@ from selfdrive.swaglog import cloudlog
 from selfdrive.config import Conversions as CV
 
 CRUISE_GAP_BP = [1., 2., 3., 4.]
-CRUISE_GAP_V = [1.3, 1.4, 1.8, 2.2]
+CRUISE_GAP_V = [1.3, 1.4, 1.5, 1.7]
 
 AUTO_TR_BP = [10.*CV.KPH_TO_MS, 70.*CV.KPH_TO_MS, 130.*CV.KPH_TO_MS]
-AUTO_TR_V = [1.3, 1.40, 1.5]
+AUTO_TR_V = [1.3, 1.35, 1.4]
 
 AUTO_TR_ENABLED = True
 AUTO_TR_CRUISE_GAP = 1
