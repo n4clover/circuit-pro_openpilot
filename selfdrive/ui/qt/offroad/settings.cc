2eb9b449de26df7d7ac9bc497eda50ba4adbc620
Circuit Pro Industries <oricalworks@gmail.com>
2021-10-16 11:26:33 -0400

    Update settings.cc

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -76,11 +76,6 @@ TogglesPanel::TogglesPanel(QWidget *parent) : ListWidget(parent) {
                                   "In this mode openpilot will ignore lanelines and just drive how it thinks a human would.",
                                   "../assets/offroad/icon_road.png",
                                   this));
-  addItem(new ParamControl("AR",
-                                            "Enable Auto Record",
-                                            "Starts recording on car start and stops on car off.",
-                                            "../assets/offroad/icon_road.png",
-                                            this));
   addItem(new ParamControl("LoggerEnabled",
                                             "Enable Logger / Uploader",
                                             "This causes slow frame time on weak hardware.",
@@ -97,8 +92,8 @@ TogglesPanel::TogglesPanel(QWidget *parent) : ListWidget(parent) {
                                             "../assets/offroad/icon_road.png",
                                             this));
   addItem(new ParamControl("LowSpeedAlerts",
-                                            "Enable TPMS Alerts",
-                                            "Enables Tire Pressure Monitoring System Alerts for Low Tire Pressure.",
+                                            "Enable Low Speed Alerts",
+                                            "Enables Low Speed alerts for cars with min steer speeds.",
                                             "../assets/offroad/icon_road.png",
                                             this));
   addItem(new ParamControl("SSCOD",
