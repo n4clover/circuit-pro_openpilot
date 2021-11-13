4787028ec0b89a0f2873cce25fcd7b76345d4bbc
Circuit Pro Industries <oricalworks@gmail.com>
2021-11-12 23:15:38 -0500

    Update carstate.py

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -200,9 +200,10 @@ class CarState(CarStateBase):
     if self.spas_enabled: # SPAS
       self.ems_366 = cp.vl["EMS_366"]
       self.ems11 = cp.vl["EMS11"]
-      self.mdps11_strang = cp_mdps.vl["MDPS11"]["CR_Mdps_StrAng"]
-      self.mdps11_stat = cp_mdps.vl["MDPS11"]["CF_Mdps_Stat"]
-      ret.mdps11Stat = cp_mdps.vl["MDPS11"]["CF_Mdps_Stat"]
+      self.eems11 = cp.vl["E_EMS11"]
+    self.mdps11_strang = cp_mdps.vl["MDPS11"]["CR_Mdps_StrAng"]
+    self.mdps11_stat = cp_mdps.vl["MDPS11"]["CF_Mdps_Stat"]
+    ret.mdps11Stat = cp_mdps.vl["MDPS11"]["CF_Mdps_Stat"]
 
 
     # scc smoother
