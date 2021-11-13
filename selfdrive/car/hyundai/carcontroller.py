d3ede1200e64acf24bb5d24d03af6135c2d19bcb
Circuit Pro Industries <oricalworks@gmail.com>
2021-11-13 00:46:51 -0500

    Fix Thanks To Desta

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -207,7 +207,7 @@ class CarController():
     if frame % 2 and CS.mdps_bus: # send clu11 to mdps if it is not on bus 0
       can_sends.append(create_clu11(self.packer, frame // 2 % 0x10, CS.mdps_bus, CS.clu11, Buttons.NONE, enabled_speed))
 
-    if pcm_cancel_cmd and (self.longcontrol) #Fix SCC stay active when op disengage
+    if pcm_cancel_cmd: #Fix SCC stay active when op disengage - Desta
       can_sends.append(create_clu11(self.packer, frame % 0x10, CS.scc_bus, CS.clu11, Buttons.CANCEL, clu11_speed))
     else:
       can_sends.append(create_mdps12(self.packer, frame, CS.mdps12))
