4361e221adc81719e0f1b301c1c93776f33971ce
Circuit Pro Industries <oricalworks@gmail.com>
2021-11-18 19:32:47 -0500

    Update safety_hyundai_community.h

--- panda/board/safety/safety_hyundai_community.h
+++ panda/board/safety/safety_hyundai_community.h
@@ -106,7 +106,7 @@ static int hyundai_community_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
     }
 
     if (addr == 914 && bus == HKG_mdps_bus) { // Read MDPS11, CR_Mdps_DrvTq : Driver Torque
-      int driver_torque = ((GET_BYTES_04(to_push) & 0x7ff) * 0.79) - 808;
+      driver_torque = ((GET_BYTES_04(to_push) & 0x7ff) * 0.79) - 808;
     }
 
     if (addr == 1056 && !OP_SCC_live) { // for cars without long control
