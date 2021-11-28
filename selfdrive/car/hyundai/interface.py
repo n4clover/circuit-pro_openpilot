56a919f451972261efb1d181accc5ec368f50bd2
Circuit Pro Industries <oricalworks@gmail.com>
2021-11-28 11:01:27 -0500

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -99,7 +99,7 @@ class CarInterface(CarInterfaceBase):
     ret.startAccel = -0.5
     ret.stopAccel = -2.0
     ret.startingAccelRate = 5.0  # brake_travel/s while releasing on restart
-    ret.stoppingDecelRate = 0.5  # brake_travel/s while trying to stop
+    ret.stoppingDecelRate = 0.65  # brake_travel/s while trying to stop
     ret.vEgoStopping = 0.8
     ret.vEgoStarting = 1.0
 
