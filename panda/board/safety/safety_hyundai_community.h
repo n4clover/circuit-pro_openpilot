786f0cba05caa57969b4bddf5f7435ca3c897c5d
Circuit Pro Industries <oricalworks@gmail.com>
2021-12-04 11:57:29 -0500

    Update safety_hyundai_community.h

--- panda/board/safety/safety_hyundai_community.h
+++ panda/board/safety/safety_hyundai_community.h
@@ -9,11 +9,11 @@ int HKG_scc_bus = -1;
 
 const struct lookup_t HYUNDAI_LOOKUP_ANGLE_RATE_UP = { // Add to each value from car controller to leave a bit of margin.
     {2., 30., 60.}, //kph
-    {16., 14.5, 12.5}};  //deg
+    {16., 15., 13.}};  //deg
 
 const struct lookup_t HYUNDAI_LOOKUP_ANGLE_RATE_DOWN = { // Add to each value from car controller to leave a bit of margin.
     {2., 30., 60.}, //kph
-    {17., 15.5, 13.5}}; //deg 
+    {17., 16., 14.}}; //deg 
 
 const int HYUNDAI_DEG_TO_CAN = 10; 
 
