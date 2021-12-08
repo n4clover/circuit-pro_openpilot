b3b8c1542cd71f45368a7096bf7cbb5f5d8124aa
Circuit Pro Industries <oricalworks@gmail.com>
2021-12-08 00:25:53 -0500

    disable until get help

--- selfdrive/locationd/paramsd.py
+++ selfdrive/locationd/paramsd.py
@@ -165,14 +165,14 @@ def main(sm=None, pm=None):
       msg.liveParameters.angleOffsetAverageStd = float(P[States.ANGLE_OFFSET])
       msg.liveParameters.angleOffsetFastStd = float(P[States.ANGLE_OFFSET_FAST])
 
-      if sm.frame % 1200 == 0 and Params().get_bool('spasEnabled'):#sm['carState'].mdps11Stat == 5:  # once a minute and SPAS enabled - TO DO - when active mdps11 state 5 - JPR
-        params = {
-          'carFingerprint': CP.carFingerprint,
-          'steerRatio': CP.steerRatio,
-          'stiffnessFactor': msg.liveParameters.stiffnessFactor,
-          'angleOffsetAverageDeg': msg.liveParameters.angleOffsetAverageDeg,
-        }
-      elif sm.frame % 1200 == 0:
+    #  if sm.frame % 1200 == 0 and Params().get_bool('spasEnabled'): #sm['carState'].mdps11Stat == 5:  # once a minute and SPAS enabled - TO DO - when active mdps11 state 5 - JPR
+    #    params = {
+    #      'carFingerprint': CP.carFingerprint,
+    #      'steerRatio': CP.steerRatio,
+    #      'stiffnessFactor': msg.liveParameters.stiffnessFactor,
+    #      'angleOffsetAverageDeg': msg.liveParameters.angleOffsetAverageDeg,
+    #    }
+      if sm.frame % 1200 == 0:
          params = {
           'carFingerprint': CP.carFingerprint,
           'steerRatio': msg.liveParameters.steerRatio,
