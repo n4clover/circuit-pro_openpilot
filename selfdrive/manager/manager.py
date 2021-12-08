682e93abb774e368b14db0c8341795a76e2dd679
Circuit Pro Industries <oricalworks@gmail.com>
2021-12-08 01:24:46 -0500

    Default to correct spas mode

--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -79,7 +79,7 @@ def manager_init():
     ("SteerLockout", "0"),
     ("spasEnabled", "0"),
     ("DynamicSpas", "0"),
-    ("SpasMode", "1"),
+    ("SpasMode", "0"),
     ("SPASDebug", "0"),
     ("CreepDebug", "0"),
   ]
