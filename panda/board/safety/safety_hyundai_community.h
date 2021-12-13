8b9a294f05f32ff83f7f05cdc97eda928bbac1ae
Circuit Pro Industries <oricalworks@gmail.com>
2021-12-13 14:04:25 -0500

    Tune spas

--- panda/board/safety/safety_hyundai_community.h
+++ panda/board/safety/safety_hyundai_community.h
@@ -9,11 +9,11 @@ int HKG_scc_bus = -1;
 
 const struct lookup_t HYUNDAI_LOOKUP_ANGLE_RATE_UP = { // Add to each value from car controller to leave a bit of margin.
     {0., 30., 60.}, //kph
-    {18, 17., 16.}};  //deg
+    {19, 18., 17.}};  //deg
 
 const struct lookup_t HYUNDAI_LOOKUP_ANGLE_RATE_DOWN = { // Add to each value from car controller to leave a bit of margin.
     {0., 30., 60.}, //kph
-    {19., 18., 17.}}; //deg 
+    {20., 19., 18.}}; //deg 
 
 const int HYUNDAI_DEG_TO_CAN = 10; 
 
