d7b76a2bdff1fb7378ed1464977048505de26090
Circuit Pro Industries <oricalworks@gmail.com>
2021-12-17 22:30:13 -0500

    Create recover_panda.sh

---
+++ selfdrive/debug/recover_panda.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Recovering Panda; Sit tight and wait for reboot..."
+pkill -f openpilot
+cd panda/board
+./recover.sh
+echo "Recovering Panda; If no reboot in 5 min try again.."
+sudo reboot
