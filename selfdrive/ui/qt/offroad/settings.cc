9ea6632696d85ab128fa7759785ed4eaf4e897ac
Circuit Pro Industries <oricalworks@gmail.com>
2022-02-02 22:50:14 -0500

    fix Settings ui on C2

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -651,7 +651,7 @@ CommunityPanel::CommunityPanel(QWidget* parent) : QWidget(parent) {
                                             this));
 
   toggles.append(new ParamControl("SteerLockout",
-                                            "Enable Higher Than 90° Lateral Control for LKAS",
+                                            "Enable More Than 90° for LKAS",
                                             "This disables the max steer limit of 90°. SPAS does not apply to this. This will cause fault on certain cars that have a 90° limit on LKAS.",
                                             "../assets/offroad/icon_road.png",
                                             this));
