aa7d722e2f806869e81076457145fb619ecf1ce8
Circuit Pro Industries <oricalworks@gmail.com>
2022-02-02 23:46:09 -0500

    fixes

--- selfdrive/controls/lib/desire_helper.py
+++ selfdrive/controls/lib/desire_helper.py
@@ -3,12 +3,12 @@ from common.realtime import DT_MDL
 from selfdrive.config import Conversions as CV
 from common.params import Params
 
-AUTO_LCA_START_TIME = 1.0
+AUTO_LCA_START_TIME = 0.15
 
 LaneChangeState = log.LateralPlan.LaneChangeState
 LaneChangeDirection = log.LateralPlan.LaneChangeDirection
 
-LANE_CHANGE_SPEED_MIN = 1 if Params().get_bool('NoMinLaneChangeSpeed') else 24 * CV.MPH_TO_MS
+LANE_CHANGE_SPEED_MIN = 24 * CV.MPH_TO_MS
 LANE_CHANGE_TIME_MAX = 10.
 
 DESIRES = {
@@ -51,7 +51,7 @@ class DesireHelper:
   def update(self, carstate, active, lane_change_prob):
     v_ego = carstate.vEgo
     one_blinker = carstate.leftBlinker != carstate.rightBlinker
-    below_lane_change_speed = v_ego < LANE_CHANGE_SPEED_MIN
+    below_lane_change_speed = v_ego < LANE_CHANGE_SPEED_MIN if not Params().get_bool('NoMinLaneChangeSpeed') else False
 
     if (not active) or (self.lane_change_timer > LANE_CHANGE_TIME_MAX) or (not one_blinker) or (not self.lane_change_enabled):
       self.lane_change_state = LaneChangeState.off
