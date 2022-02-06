74367b8bbd8f8e8f17fdc4914a670aaf40e660f4
Circuit Pro Industries <oricalworks@gmail.com>
2022-02-06 11:11:24 -0500

    Update safety_hyundai_community.h

--- panda/board/safety/safety_hyundai_community.h
+++ panda/board/safety/safety_hyundai_community.h
@@ -112,7 +112,7 @@ static int hyundai_community_rx_hook(CANPacket_t *to_push) {
       //puts("   Driver Torque   "); puth(driver_torque); puts("\n");
     } 
 
-    if (HKG_scc_bus != 1 && HKG_scc_bus != 2 && !OP_SCC_live) { //Radar off can or disabled - JPR
+    if (HKG_scc_bus != 1 && HKG_scc_bus != 2 && OP_SCC_live) { //Radar off can or disabled - JPR
       // ACC steering wheel buttons
       if (addr == 1265) {
         int button = GET_BYTE(to_push, 0) & 0x7U;
@@ -128,7 +128,7 @@ static int hyundai_community_rx_hook(CANPacket_t *to_push) {
             break;  // any other button is irrelevant
         }
       }
-    } /*else {
+    } else {
       if (addr == 1056 && !OP_SCC_live) { // for cars without long control
         // 2 bits: 13-14
         int cruise_engaged = GET_BYTES_04(to_push) & 0x1; // ACC main_on signal
@@ -144,7 +144,7 @@ static int hyundai_community_rx_hook(CANPacket_t *to_push) {
       }
 
       // cruise control for car without SCC
-      if (addr == 608 && bus == 0 && HKG_scc_bus == -1 && !OP_SCC_live) {
+      /*if (addr == 608 && bus == 0 && HKG_scc_bus == -1 && !OP_SCC_live) {
         // bit 25
         int cruise_engaged = (GET_BYTES_04(to_push) >> 25 & 0x1); // ACC main_on signal
         if (cruise_engaged && !cruise_engaged_prev) {
@@ -156,8 +156,8 @@ static int hyundai_community_rx_hook(CANPacket_t *to_push) {
             controls_allowed = 0;
         }
         cruise_engaged_prev = cruise_engaged;
-      }
-    }*/
+      }*/
+    }
 
     // sample wheel speed, averaging opposite corners
     if (addr == 902 && bus == 0) {
