590cc01d23a878e12f24a0568b3dff2e59427645
Circuit Pro Industries <oricalworks@gmail.com>
2022-02-10 13:48:41 -0500

    Update controlsd.py

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -425,7 +425,7 @@ class Controls:
       self.v_cruise_kph = update_v_cruise(self.v_cruise_kph, CS.buttonEvents, self.button_timers, self.enabled, self.is_metric)
     elif CS.cruiseState.enabled and self.CP.openpilotLongitudinalControl:
     #  self.v_cruise_kph = CS.cruiseState.speed * CV.MS_TO_KPH
-      SccSmoother.update_cruise_buttons(self, CS, self.CP.openpilotLongitudinalControl, self.enabled)
+      SccSmoother.update_cruise_buttons(self, CS, self.CP.openpilotLongitudinalControl)
 
     # decrement the soft disable timer at every step, as it's reset on
     # entrance in SOFT_DISABLING state
