d1fd3f30a8ba4d84795640beca329535082a3ea3
CPI <oricalworks@gmail.com>
2022-02-14 13:08:44 -0500

    Update carcontroller.py

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -256,7 +256,7 @@ class CarController():
         self.gapcount = 0
       self.gapsetting = self.gapsettingdance
     else:
-      d = self.scc_smoother.get_lead(controls.sm).dRel
+      d = CS.lead_distance
       self.gapsettingdance = 1 if d < 25 else 2 if d < 40 else 3 if d < 60 else 4
     # scc smoother
     self.scc_smoother.update(enabled, can_sends, self.packer, CC, CS, frame, controls)
