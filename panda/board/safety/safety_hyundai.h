fbb4855c084bd3f75716f80ad4a45840a5d503d1
james5294 <james5294@msn.com>
2022-02-20 16:01:38 -0500

    Update safety_hyundai.h

--- panda/board/safety/safety_hyundai.h
+++ panda/board/safety/safety_hyundai.h
@@ -381,6 +381,7 @@ static const addr_checks* hyundai_legacy_init(int16_t param) {
   hyundai_legacy = true;
   hyundai_longitudinal = false;
   radar_disable = GET_FLAG(param, HYUNDAI_PARAM_LONGITUDINAL);
+  if (radar_disable) {heartbeat_disabled = true;}
   hyundai_ev_gas_signal = GET_FLAG(param, HYUNDAI_PARAM_EV_GAS);
   hyundai_hybrid_gas_signal = !hyundai_ev_gas_signal && GET_FLAG(param, HYUNDAI_PARAM_HYBRID_GAS);
   hyundai_rx_checks = (addr_checks){hyundai_legacy_addr_checks, HYUNDAI_LEGACY_ADDR_CHECK_LEN};
