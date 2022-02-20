2b23fdb8e26b5b0fd7c7d6ff52724d3c15e8b4c5
james5294 <james5294@msn.com>
2022-02-20 15:50:39 -0500

    Revert "test"
    This reverts commit b20d4b57fbabc365e91ec08259b62a91658f4152.

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -565,7 +565,7 @@ class CarInterface(CarInterfaceBase):
     ret.pcmCruise = not ret.radarOffCan or not ret.radarDisable
 
     if ret.openpilotLongitudinalControl and (ret.radarDisable or ret.radarOffCan):
-      ret.safetyConfigs[0].safetyParam |= Panda.FLAG_RADAR_DISABLE
+      ret.safetyConfigs[0].safetyParam |= Panda.FLAG_HYUNDAI_LONG
     
     # SPAS
     ret.spasEnabled = Params().get_bool('spasEnabled')
