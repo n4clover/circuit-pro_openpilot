6cfa5b05a185bed46e7d40d98a8c03343e0539a3
Circuit Pro Industries <oricalworks@gmail.com>
2022-03-05 09:35:08 -0500

    fix MDPS11 signals and checks for SPAS/RSPA

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -575,12 +575,12 @@ class CarState(CarStateBase):
             ("CR_Vcu_AccPedDep_Pos", "E_EMS11", 0),
           ]
           checks += [("E_EMS11", 100)]
-        elif CP.mdpsBus == 0:
-          signals += [
-            ("CR_Mdps_StrAng", "MDPS11", 0),
-            ("CF_Mdps_Stat", "MDPS11", 0),
-          ]
-          checks += [("MDPS11", 100)]
+      elif CP.mdpsBus == 0:
+        signals += [
+          ("CR_Mdps_StrAng", "MDPS11", 0),
+          ("CF_Mdps_Stat", "MDPS11", 0),
+        ]
+        checks += [("MDPS11", 100)]
     if Params().get_bool("HyundaiNaviSL"):
       signals += [
         ("SpeedLim_Nav_Clu", "Navi_HU", 0),
