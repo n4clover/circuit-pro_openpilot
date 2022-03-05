abbacbaffe194aabad43b289b72643eaa7098e5b
Circuit Pro Industries <oricalworks@gmail.com>
2022-03-05 03:52:09 -0500

    test 2

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -280,10 +280,10 @@ class Controls:
                           #pandaState.safetyParam != self.CP.safetyConfigs[i].safetyParam or \
                           #pandaState.unsafeMode != self.CP.unsafeMode
       else:
-        safety_mismatch = pandaState.safetyModel not in IGNORED_SAFETY_MODES
+        #safety_mismatch = pandaState.safetyModel not in IGNORED_SAFETY_MODES
 
-      if safety_mismatch or self.mismatch_counter >= 200:
-        self.events.add(EventName.controlsMismatch)
+      #if safety_mismatch or self.mismatch_counter >= 200:
+      #  self.events.add(EventName.controlsMismatch)
 
       if log.PandaState.FaultType.relayMalfunction in pandaState.faults:
         self.events.add(EventName.relayMalfunction)
